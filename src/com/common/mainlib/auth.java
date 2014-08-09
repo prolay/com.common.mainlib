78e051f285b2debf9e3307787ec5d2089e89da42
Prolay Sarkar <prolay@ymail.com>
2014-08-09 12:22:56 +0530

    first commit

---
+++ src/com/common/mainlib/auth.java
@@ -0,0 +1,16 @@
+package com.common.mainlib;
+/*
+ * Class for validation process
+ */
+public class auth {
+
+	public boolean validate(String user){
+		if (user == "Prolay"){
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
